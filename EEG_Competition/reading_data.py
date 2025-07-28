60bd4d196acdddcae9d505afdce1facb3f0d1178
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2025-07-28 23:02:13 +0300

    "information of data"

--- EEG_Competition/reading_data.py
+++ EEG_Competition/reading_data.py
@@ -2,29 +2,27 @@
 import mne.io as io
 
 file_path = 'C:/Users\HP\Downloads\Compressed\sub-NDARFW972KFQ\eeg\sub-NDARFW972KFQ_task-DiaryOfAWimpyKid_eeg.set'  # Replace with the actual path to your file
-file_path = "C:/Users\HP\Downloads\Compressed\sub-NDARFW972KFQ\eeg\sub-NDARFW972KFQ_task-contrastChangeDetection_run-3_eeg.set"  # Replace with the actual path to your file
 
 raw = io.read_raw_eeglab(file_path, preload=True)
 raw.info
-chan_names = raw.ch_names
-Sampling_frequency = int(raw.info['sfreq'])
 
-import mne.io as io
-import matplotlib.pyplot as plt
+'''
+<Info | 8 non-empty values
+ bads: []
+ ch_names: E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, ...
+ chs: 129 EEG
+ custom_ref_applied: False
+ dig: 132 items (3 Cardinal, 129 EEG)
+ highpass: 0.0 Hz
+ lowpass: 250.0 Hz
+ meas_date: unspecified
+ nchan: 129
+ projs: []
+ sfreq: 500.0 Hz
+>
 
-print(f"Successfully loaded {file_path}")
-print(f"Number of channels: {len(raw.ch_names)}")
-print(f"Sampling frequency: {raw.info['sfreq']} Hz")
-print(f"Duration: {raw.times[-1]:.2f} seconds")
+'''
 
-# Access the data (e.g., first 5 channels, first 2 seconds)
-data, times = raw[:5, :Sampling_frequency]
-print(f"Shape of data (first 5 channels, 2 seconds): {data.shape}")
-
-# Plot the raw data (first few channels)
-raw.plot(n_channels=5, scalings='auto', title='Raw EEG Data')
-plt.show()
-
-# Plot the power spectral density
-raw.plot_psd(fmax=50, average=True, spatial_colors=False)
-plt.show()
+print(f"Number of channels: {len(raw.ch_names)}") # Number of channels: 129
+print(f"Sampling frequency: {raw.info['sfreq']} Hz") # Sampling frequency: 500.0 Hz
+print(f"Duration: {raw.times[-1]:.2f} seconds") # Duration: 118.93 seconds
