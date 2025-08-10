e9e3250a3d54a9b3a6ded64335eb89db594f134a
seyedhasanmirhoseini1367 <seyedhasan.mirhoseini1367@gmail.com>
2025-08-10 11:12:25 +0300

    "Targets of challenge 1: reaction time and hit accuracy"

---
+++ EEG_Competition/Targets_Challenge1.py
@@ -0,0 +1,22 @@
+
+
+def reaction_time(df):
+    rt_target = []
+    # Find indices where 'feedback' contains 'face' (smiley_face or sad_face)
+    target_indices = df.index[df['feedback'].str.contains('face', na=False)].tolist()
+
+    for idx in target_indices:
+        # Check if the current row's 'value' ends with 'buttonPress'
+        press_buttons = df[(df.index == idx) & (df['value'].str.contains('buttonPress'))]
+
+        # Check if the previous row's 'value' is a target event (left_target or right_target)
+        prev_value = df.loc[idx - 1, 'value']
+        if prev_value not in ['left_target', 'right_target']:
+            continue
+
+        if not press_buttons.empty:
+            rt = press_buttons.iloc[0]['onset'] - df.loc[idx-1, 'onset']
+            target = press_buttons.iloc[0]['feedback']
+            rt_target.append((np.round(rt, 3), target))
+
+    return rt_target
